0fd6940b1265c0a873a73782faf0b9e1a1c26d49
FreyDv <ldfm0401@gmail.com>
2024-09-27 09:30:31 -0400

    BE-2: Added some helpful log during debug connection to DB

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SwaggerModule } from '@nestjs/swagger';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { sequelizeConfig } from './db/sequelizeModuleOptions';
+import { Sequelize } from 'sequelize-typescript';
 
 @Module({
   imports: [SwaggerModule, SequelizeModule.forRoot(sequelizeConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private sequelize: Sequelize) {}
+
+  async onModuleInit() {
+    await this.sequelize.authenticate();
+  }
+}
--- be/src/db/sequelizeModuleOptions.ts
+++ be/src/db/sequelizeModuleOptions.ts
@@ -9,4 +9,23 @@ export const sequelizeConfig: SequelizeModuleOptions = {
   password: config.db.password,
   database: config.db.name,
   models: [],
+  logging: console.debug,
+  dialectOptions: {
+    connectTimeout: 60000, // Adjust connection timeout
+  },
+  hooks: {
+    beforeConnect: (config) => {
+      console.log(
+        `Attempting to connect to mysql://${config.username}:${config.password.replace(/.+/, '*****')}@${config.host}:${config.port}/${config.database}\n as mysql://<username>:<password>@<host>:<port>/<database>`,
+      );
+    },
+    afterConnect: (connection) => {
+      console.log(
+        `Authorized to MySQL ${connection['authorized'] ? 'Successfully' : 'Failed'}!`,
+      );
+    },
+    beforeDisconnect: () => {
+      // console.log('Disconnecting from MySQL...');
+    },
+  },
 };
