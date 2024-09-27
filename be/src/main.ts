b0ae246ca4e09d52d4e59f082593fd2d3d90ab97
FreyDv <ldfm0401@gmail.com>
2024-09-27 07:30:35 -0400

    BE-1: Added Swagger OpenApi configuration

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,17 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Login-User API')
+    .setDescription(
+      'Demonstration of code style based on Login and authorized User CRUD',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
