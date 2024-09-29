eeae8122ba1a4664e7b2bf757a5ad071c1524a4a
FreyDv <ldfm0401@gmail.com>
2024-09-29 05:37:02 -0400

    BE-4 Added TokenService and TokenModule

---
+++ be/src/common/token/token.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PayloadJWTInterface } from '../payloadJWT.interface';
+
+@Injectable()
+export class TokenService {
+  constructor(private jwtService: JwtService) {}
+
+  async verify(
+    jwt: string,
+  ): Promise<{ isValid: boolean; payload: PayloadJWTInterface }> {
+    const payload = await this.jwtService
+      .verifyAsync<PayloadJWTInterface>(jwt)
+      .catch((err) => {
+        throw new UnauthorizedException(err);
+      });
+
+    console.log(payload);
+    return { isValid: true, payload: payload };
+  }
+
+  async sign(payloadJWTInterface: PayloadJWTInterface): Promise<string> {
+    return this.jwtService.signAsync(payloadJWTInterface);
+  }
+}
