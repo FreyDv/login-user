59755d5da88750d1730ec67c8774c6113564d68c
FreyDv <ldfm0401@gmail.com>
2024-09-29 17:38:25 -0400

    BE-6 Made Documentation more user-friendly and setup open-api save mechanism in FE repo

--- be/src/app.controller.ts
+++ be/src/app.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Internal Server Controller')
 @Controller()
 export class AppController {
+  @ApiResponse({ status: HttpStatus.OK, description: 'OK' })
   @Get('/ping')
   getHello(): string {
     return `pong_${Date.now()}`;
