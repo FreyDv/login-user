c695d4edf22b1f99cf28a04faff53f49f2463ed6
FreyDv <ldfm0401@gmail.com>
2024-09-29 03:04:21 -0400

    BE-3: Temporary fix unit tests with not relevant test case. Left TODO

--- be/src/user/user.service.spec.ts
+++ be/src/user/user.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { UserModel } from './entities/user.model';
+
+const testUserModel = {
+  id: '',
+  email: '',
+  name: '',
+  secondName: '',
+  birthDate: new Date(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(UserModel),
+          useValue: {
+            create: jest.fn(() => [testUserModel]),
+            findOne: jest.fn(() => [testUserModel]),
+            findByPk: jest.fn(() => [testUserModel]),
+            update: jest.fn(() => [testUserModel]),
+            destroy: jest.fn(() => 1),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -15,4 +39,6 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  //TODO cover UserService method by unit test
 });
